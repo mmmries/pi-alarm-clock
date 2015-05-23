06c30da364774528bd407bc67574f47bb4e6a621
hqmq <michael@riesd.com>
2015-05-23 15:55:37 -0600

    change the scheduler to watch the current time

--- lib/blinky/scheduler.ex
+++ lib/blinky/scheduler.ex
@@ -11,7 +11,7 @@ defmodule Blinky.Scheduler do
   def blink(pid), do: GenServer.cast(pid, :blink)
 
   ## GenServer callbacks
-  @interval 2000
+  @interval 10_000
 
   def init(state) when state in [:idle, :keep_sleeping, :time_to_wakeup] do
     {:ok, state, @interval}
@@ -26,19 +26,41 @@ defmodule Blinky.Scheduler do
   end
 
   def handle_info(:timeout, :idle) do
-    Logger.debug "SCH idle -> keep_sleeping"
-    Blinky.Gpio.turn_on(:keep_sleeping)
-    {:noreply, :keep_sleeping, @interval}
+    case between(5,6) do
+    true ->
+      Logger.debug "SCH idle -> keep_sleeping"
+      Blinky.Gpio.turn_on(:keep_sleeping)
+      {:noreply, :keep_sleeping, @interval}
+    false ->
+      {:noreply, :idle, @interval}
+    end
   end
   def handle_info(:timeout, :keep_sleeping) do
-    Logger.debug "SCH keep_sleeping -> time_to_wakeup"
-    Blinky.Gpio.turn_off(:keep_sleeping)
-    Blinky.Gpio.turn_on(:time_to_wakeup)
-    {:noreply, :time_to_wakeup, @interval}
+    case between(5,6) do
+    true ->
+      {:noreply, :keep_sleeping, @interval}
+    false ->
+      Logger.debug "SCH keep_sleeping -> time_to_wakeup"
+      Blinky.Gpio.turn_off(:keep_sleeping)
+      Blinky.Gpio.turn_on(:time_to_wakeup)
+      {:noreply, :time_to_wakeup, @interval}
+    end
   end
   def handle_info(:timeout, :time_to_wakeup) do
-    Logger.debug "SCH time_to_wakeup -> keep_sleeping"
-    Blinky.Gpio.turn_off(:time_to_wakeup)
-    {:noreply, :idle, @interval}
+    case between?(7,8)
+    true ->
+      {:noreply, :time_to_wakeup, @interval}
+    false ->
+      Logger.debug "SCH time_to_wakeup -> keep_sleeping"
+      Blinky.Gpio.turn_off(:time_to_wakeup)
+      {:noreply, :idle, @interval}
+    end
+  end
+
+  ## Private Interface
+
+  defp between?(min, max) do
+    {h, _m, _s} = :erlang.time()
+    h >= min && h <= max
   end
 end
